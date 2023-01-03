b82158f3e109dc590fb370e2a6958a8911ce3cc3
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-01-03 12:21:31 -0500

    Exporting types.

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,16 @@
 // Imports
 //
 
-export * from "./classes/DocumentElement.js";
-export * from "./classes/DocumentPlaceholder.js";
-
 import { DocumentElement } from "./classes/DocumentElement.js";
 import { DocumentPlaceholder } from "./classes/DocumentPlaceholder.js";
 
 //
 // Exports
 //
+
+export * from "./types/Child.js";
+export * from "./types/DocumentPlaceholderReplacements.js";
+export * from "./types/TagName.js";
 
 export
 {
