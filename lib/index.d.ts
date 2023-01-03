240a1e15992ca7f3966394e3cf5e7320004d1b8f
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-01-03 12:03:08 -0500

    Removing lib files I accidentally committed.

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./classes/DocumentElement.js";
-export * from "./classes/DocumentPlaceholder.js";
-import { DocumentElement } from "./classes/DocumentElement.js";
-import { DocumentPlaceholder } from "./classes/DocumentPlaceholder.js";
-export { DocumentElement as DE, DocumentPlaceholder as DP, };
